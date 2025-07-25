206b77ac51f3e57af2817d705761d38d97403398
Oleksandra <olexandra.momotiuk@gmail.com>
2025-07-25 13:39:44 +0300

    Finish hw

---
+++ src/js/2-form.js
@@ -0,0 +1,44 @@
+const form = document.querySelector('.feedback-form');
+const formData = {
+  message: '',
+  email: '',
+};
+form.addEventListener('input', handleFormInput);
+form.addEventListener('submit', handleFormSubmit);
+document.addEventListener('DOMContentLoaded', handleDOMLoad);
+
+function handleFormInput(e) {
+  formData.email = e.currentTarget.elements.email.value;
+  formData.message = e.currentTarget.elements.message.value;
+  saveToLS('feedback-form-state', formData);
+}
+function handleFormSubmit() {
+  if (formData.email.trim() === '' || formData.message.trim() === '') {
+    alert('Fill all fields please');
+  } else {
+    console.log(formData);
+    localStorage.removeItem('feedback-form-state');
+    form.reset();
+  }
+}
+function handleDOMLoad() {
+  const lsData = getFromLS('feedback-form-state');
+  try {
+    form.elements.email.value = lsData.email;
+    form.elements.message.value = lsData.message;
+    formData = lsData;
+  } catch {}
+}
+function saveToLS(key, value) {
+  const jsonData = JSON.stringify(value);
+  localStorage.setItem(key, jsonData);
+}
+function getFromLS(key) {
+  const jsonData = localStorage.getItem(key);
+  try {
+    const data = JSON.parse(jsonData);
+    return data;
+  } catch {
+    return jsonData;
+  }
+}
